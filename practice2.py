b47d9ca7b78b2d59af914b1e298969a5bb6977e8
Tabaldiev77 <tabaldievislam256@gmail.com>
2023-10-27 19:49:59 +0600

    birinchi

---
+++ practice2.py
@@ -0,0 +1,38 @@
+
+# a = float(input("San jaz:"))
+# b = float(input("San jaz:"))
+# save = (a*b) /2
+# print(float(save))
+
+
+# a = float(input("San jaz:")) 
+# s = a- 32
+# print(s*5/9)
+
+
+
+
+# a = float (input("it zhashy :"))
+# if  a<= 2 :
+#    print(a*10.5)
+# else:
+#     print(21+(a-2)*4)
+
+
+
+
+# while True:
+#   a = input("Напиши слово:")
+# d = "синий"
+# if a.lower() in d :
+#     print("Yes")
+#     break
+# else:
+#     print("No")
+
+
+# a = input("Soz zhaz :")
+# b = input("Soz zhaz :")
+# s = input("Soz zhaz :")
+ 
+# print(min(a , b, s, key=len)) ,max(a, b, s, key =len)
